37df5875988b6b3ff5cd4a5edbffde8ccb73e74e
cartoon-raccoon <shaughn.chan@gmail.com>
2020-07-23 02:13:38 +0800

    added priority to add(), additional error handling

--- things2do.py
+++ things2do.py
@@ -10,11 +10,14 @@ def cli(all):
 @cli.command() #add command
 @click.argument('task')
 @click.option('-d', '--deadline', default=None, help='The deadline for the task.')
+@click.option('-p', '--priority', default='Low', 
+    help='Urgency of the task. Accepted values: Low, Medium, High.')
 @click.option('-r', '--remindme', default=None, 
     help='Specify a reminder in hours before the deadline.')
 @click.option('--nodelete', default=False, is_flag=True, 
     help='Set this flag to prevent t2d from auto-deleting this task.')
-def add(task, deadline, remindme, nodelete):
+def add(task, deadline, priority, remindme, nodelete):
+    #setting internal vars from fxn args
     no_delete = nodelete
 
     #handling invalid reminder input because click doesn't check for it
@@ -26,6 +29,10 @@ def add(task, deadline, remindme, nodelete):
             no_delete = True
         reminder=None
         click.echo("Invalid reminder set, defaulting to no reminder.")
+
+    if priority.lower() in ['low', 'medium', 'mid', 'high']:
+        click.echo("Invalid priority set, defaulting to low.")
+        priority = 'low'
     
     click.echo(f"\nSUMMARY:\nSet task: {task}")
 
@@ -37,6 +44,8 @@ def add(task, deadline, remindme, nodelete):
         click.echo("No deadline set, cannot set reminder.")
     else:
         click.echo(f"Reminder set for {reminder} hour(s) before deadline")
+    
+    click.echo(f"Priority set to {priority}")
     
     #logic control for deadline and auto-delete relationship
     if not deadline and not no_delete:
@@ -61,7 +70,8 @@ def edit(task):
 
 @cli.command()
 @click.argument('task')
-@click.option('-r', '--regex', is_flag=True, default=False)
+@click.option('-r', '--regex', is_flag=True, default=False,
+    help='Treat the argument as a regular expression.')
 def search(task, regex):
     if regex:
         click.echo(f"Searching for tasks matching regex {task}")
